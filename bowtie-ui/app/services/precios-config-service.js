912403c1513d33a51b3a23036d92d9ea90ae11cb
Federico Chahian <federico.chahian@gmail.com>
2015-11-29 15:34:26 -0300

    Configuración de precios

--- bowtie-ui/app/services/precios-config-service.js
+++ bowtie-ui/app/services/precios-config-service.js
@@ -4,7 +4,31 @@ define(['app'], function (app) {
 
 			var service = {};
 
+			service.getCategorias = getCategorias;
+			service.saveGrupo = saveGrupo;
+
         	return service;
+
+        	function getCategorias() {
+	        	var callback = $q.defer();
+        		$http({
+					  method: 'GET',
+					  url: 'http://ec2-52-11-118-155.us-west-2.compute.amazonaws.com/api/grupo/get'
+					}).then(function successCallback(response) 
+					{						
+			      		callback.resolve(response.data);
+					});
+				return callback.promise;
+	        }
+
+	        function saveGrupo(grupo) {
+	        	return $http.post("http://ec2-52-11-118-155.us-west-2.compute.amazonaws.com/api/grupo/post",
+			        grupo)
+			        .then(function (response) {
+			            return response;
+			        });
+				
+	        }
         	
 		}
 	]);
